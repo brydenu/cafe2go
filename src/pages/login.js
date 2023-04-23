be6f9176c1d582fb6af2fe457f201a05c1cfe5c7
Bryden Uyehara <brydenu@gmail.com>
2023-04-23 13:30:28 -0700

    Begin implementing TailwindCSS styles to login form

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,4 @@
 import Header from 'components/header/header'
-import styles from '@/styles/Home.module.css'
 
 export default function Login() {
 
@@ -7,17 +6,22 @@ export default function Login() {
     return (
         <>
         <Header />
-        <main className={styles.main}>
-            <form>
-                <div>
-                    <label>Username</label>
-                    <input />
-                </div>
-                <div>
-                    <label>Password</label>
-                    <input type="password" />
-                </div>
-            </form>
+        <main className="w-full max-w-xs">
+            {/* <div className="w-full max-w-xs bg-gray">    */}
+                <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" for="username">Username</label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                    </div>
+                    <div>
+                        <label className="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" />
+                    </div>
+                    <div className="flex justify-end">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" >Sign in</button>
+                    </div>
+                </form>
+            {/* </div> */}
         </main>
         </>
     )
