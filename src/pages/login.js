c0f207d708e03d0173e51e66bd7b3491980942b9
Bryden Uyehara <brydenu@gmail.com>
2023-04-23 14:06:23 -0700

    Added logo and title to login + styling adjustments

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,5 @@
 import Header from 'components/header/header'
+import Image from 'next/image';
 
 export default function Login() {
 
@@ -6,7 +7,17 @@ export default function Login() {
     return (
         <>
         <Header />
-        <main className="w-full h-screen flex items-center justify-center bg-blfs-blue">
+        <main className="w-full h-screen flex flex-col items-center justify-center bg-gray-200">
+            <div className="w-full flex justify-center items-center my-10">
+                <Image 
+                    priority
+                    src="/images/BS-Symbol-BS-Master-TM.svg"
+                    height={75}
+                    width={75}
+                    alt="Biolife Solutions logo"
+                />
+                <h1 className="mx-4 font-bold text-4xl">BLFS Cafe</h1>
+            </div>
             <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-2/5 h-1/3 flex flex-col justify-center">
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" for="username">Username</label>
@@ -17,7 +28,7 @@ export default function Login() {
                     <input className="shadow appearance-none border-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" />
                 </div>
                 <div className="flex justify-end">
-                    <button className="bg-blfs-teal hover:bg-blfs-blue text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Sign in</button>
+                    <button className="bg-blfs-teal hover:bg-blfs-blue text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Sign in</button>
                 </div>
             </form>
         </main>
