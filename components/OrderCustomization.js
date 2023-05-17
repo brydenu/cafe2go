31097e42c1b49f32ab764742ee6dd3b88170d554
brydenu <brydenu@gmail.com>
2023-05-17 14:58:47 -0700

    Added formatter for data

--- components/OrderCustomization.js
+++ components/OrderCustomization.js
@@ -1,9 +1,22 @@
 import { useState, useEffect } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { ChevronUpDownIcon } from '@heroicons/react/20/solid';
+import IngredientOption from "./IngredientOption";
+import DrinkOption from "./DrinkOption";
 
-export default function OrderCustomization({ label, options }) {
-    const [selectedOption, setSelectedOption] = useState(options[0])
+export default function OrderCustomization({ label, options, ingredient=true }) {
+    const formattedOptions = ingredient ? options.map((option) => ({
+        id: option.ingredient_id,
+        disabled: !option.in_stock,
+        name: option.ingredient_name,
+    })) : options.map((option) => ({
+        id: option.drink_id,
+        disabled: !option.in_stock,
+        name: option.drink_name
+    }));
+    
+    const [selectedOption, setSelectedOption] = useState(formattedOptions[0]);
+
 
     return (
         <div className="w-full flex flex-nowrap justify-between my-2">
@@ -11,7 +24,7 @@ export default function OrderCustomization({ label, options }) {
             <div className="">
                 <Listbox value={selectedOption} onChange={setSelectedOption}>
                     <Listbox.Button className="border text-xl pr-1 pl-3 py-1 rounded flex flex-nowrap justify-between items-center gap-3">
-                        {selectedOption.label}
+                        {selectedOption.name}
                         <ChevronUpDownIcon
                             className="h-5 w-5 text-gray-400"
                             aria-hidden="true"
@@ -26,14 +39,20 @@ export default function OrderCustomization({ label, options }) {
                         leaveTo="transform scale-95 opacity-0"
                     >
                         <Listbox.Options className="z-10 shadow absolute bg-white rounded max-h-64 overflow-auto">
-                            {options.map((option) => (
+                            {formattedOptions.map((option) => (
                                 <Listbox.Option
                                     key={option.id}
                                     value={option}
-                                    disabled={option.unavailable}
+                                    disabled={option.disabled}
                                     className="hover:cursor-pointer px-10 py-1 rounded hover:bg-blfs-teal hover:text-white text-center text-xl"
                                 >
-                                    {option.label}
+                                    {({active}) => (
+                                        <div
+                                            className={`${active && "bg-blfs-teal text-white"}`}
+                                        >
+                                            {option.name}
+                                        </div>
+                                    )}
                                 </Listbox.Option>
                             ))}
                         </Listbox.Options>
