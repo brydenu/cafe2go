5061283cbd2c26bb4e4f3a3292040a5f91d41241
brydenu <brydenu@gmail.com>
2023-05-19 11:14:10 -0700

    /order now hits API on new drink selection to get all required customizations per drink

--- src/pages/order.js
+++ src/pages/order.js
@@ -1,20 +1,39 @@
 import { useState, useEffect } from "react";
 import Header from "components/Header";
-import OrderCustomization from "components/OrderCustomization";
 import getIngredients from "helpers/getIngredients";
 import getMenu from "helpers/getMenu";
 import getCustomizations from "helpers/getCustomizations";
+import DrinkOption from "components/DrinkOption";
+import IngredientOption from "components/IngredientOption";
+import QuantityOption from "components/QuantityOption";
+
 
 export default function Order({ menu, ingredients, customizations }) {
   const [bevType, setBevType] = useState("coffee");
+  const [selectedDrink, setSelectedDrink] = useState(menu[0]);
+  const [currentDrinkCustomizations, setCurrentDrinkCustomizations] = useState([]);
 
   const bevTabClasses = "w-40 text-white text-xl px-5 py-2 rounded-xl duration-200";
   const selectedOption = "bg-blfs-teal " + bevTabClasses;
   const unselectedOption = "bg-blfs-blue hover:bg-blfs-teal/50  " + bevTabClasses;
 
-  // console.log("ingredients print:", ingredients);
-  // console.log("menu print:", menu);
+  console.log("ingredients print:", ingredients);
+  console.log("menu print:", menu);
   console.log("customizations print:", customizations);
+
+  useEffect(() => {
+    const getCurrentDrinkCustomizationOptions = async () => {
+      console.log("useEffect", `api/customizations/${selectedDrink.menu_id}`);
+
+      const res = await fetch(`api/customizations/1`);
+      const { customizations } = await res.json()
+      setCurrentDrinkCustomizations(customizations);
+    };
+
+    getCurrentDrinkCustomizationOptions();
+  }, [selectedDrink]);
+
+  console.log("currentDrinkCustomizations:", currentDrinkCustomizations);
     
   const handleChangeTab = (e) => {
     e.preventDefault();
@@ -38,14 +57,15 @@ export default function Order({ menu, ingredients, customizations }) {
             <button type="button" value="other" className={bevType === "other" ? selectedOption : unselectedOption} onClick={handleChangeTab}>Other</button>
           </div>
           <div className="w-full flex flex-col w-full bg-white justify-center mb-2 px-8">
-            {/* {customizeOptions.map((customization) => (
-              <>
-                <OrderCustomization key={customization.id} label={customization.label} options={customization.options} />
-                <hr />
-              </>
-            ))} */}
-            <OrderCustomization label="Drink" options={menu} ingredient={false} />
-            <OrderCustomization label="Milk" options={ingredients.milks} />
+            <DrinkOption selectedDrink={selectedDrink} setSelectedDrink={setSelectedDrink} menu={menu} />
+            { currentDrinkCustomizations.length > 0 && 
+              currentDrinkCustomizations.map((customization) => (
+                (customization.customization_ingredient === "number" || customization.customization_ingredient === "amount") ? 
+                <QuantityOption customization={customization} />
+                :
+                <IngredientOption customization={customization} />
+              ))
+            }
           </div>
           <button className="w-30 text-white rounded-xl bg-green-600 hover:bg-green-500 duration-200 self-end font-bold text-lg mx-10 my-2 px-4 py-2" onClick={handleSubmit}>Submit Order</button>
         </div>
