b421e7aa358f5a708dd191004c1e644b8cc26e95
brydenu <brydenu@gmail.com>
2023-05-19 11:10:19 -0700

    Option components now hit api endpoint to get options after /order gives it the category. Created new QuantityOption component for selectors that use numbers (e.g. number of pumps) or amounts (e.g. extra ice)

--- components/IngredientOption.js
+++ components/IngredientOption.js
@@ -1,26 +1,68 @@
+import { useState, useEffect } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { ChevronUpDownIcon } from '@heroicons/react/20/solid';
 
-export default function IngredientOption({ options }) {
+export default function IngredientOption({ customization }) {
+    const [options, setOptions] = useState([])
+    const [selectedOption, setSelectedOption] = useState({});
+
+    const { customization_name, customization_label, customization_ingredient } = customization;
+
+    useEffect(() => {
+        const getOptions = async () => {
+            const res = await fetch(`api/ingredients/${customization_ingredient}`);
+            const {ingredientOptions} = await res.json();
+            setOptions(ingredientOptions);
+            setSelectedOption(ingredientOptions[0]);
+            console.log(ingredientOptions)
+        }
+
+        getOptions();
+    },[]);
+
+    console.log("options:", options);
 
     return (
-        <Listbox.Options className="z-10 shadow absolute bg-white rounded max-h-64 overflow-auto">
-            {options.map((option) => (
-                <Listbox.Option
-                    key={option.ingredient_id}
-                    value={option}
-                    disabled={!option.in_stock}
-                    className="hover:cursor-pointer px-10 py-1 rounded hover:bg-blfs-teal hover:text-white text-center text-xl"
-                >
-                    {({active}) => (
-                        <div
-                            className={`${active && "bg-blfs-teal text-white"}`}
-                        >
-                            {option.ingredient_name}
-                        </div>
-                    )}
-                </Listbox.Option>
-            ))}
-        </Listbox.Options>
+        <div className="w-full flex flex-nowrap justify-between my-2">
+            <label className="text-xl">{customization_label}</label>
+            <div className="">
+                <Listbox value={selectedOption} onChange={setSelectedOption}>
+                    <Listbox.Button className="border text-xl pr-1 pl-3 py-1 rounded flex flex-nowrap justify-between items-center gap-3">
+                        {selectedOption && selectedOption.ingredient_name}
+                        <ChevronUpDownIcon
+                            className="h-5 w-5 text-gray-400"
+                            aria-hidden="true"
+                        />
+                    </Listbox.Button>
+                    <Transition
+                        enter="transition duration-200 ease-out"
+                        enterFrom="transform scale-95 opacity-0"
+                        enterTo="transform scale-100 opacity-100"
+                        leave="transition duration-100 ease-out"
+                        leaveFrom="transform scale-100 opacity-100"
+                        leaveTo="transform scale-95 opacity-0"
+                    >
+                        <Listbox.Options className="z-10 shadow absolute bg-white rounded max-h-64 overflow-auto">
+                            {options.length > 1 && options.map((option) => (
+                                <Listbox.Option
+                                    key={option.ingredient_id}
+                                    value={option}
+                                    disabled={!option.in_stock}
+                                    className="hover:cursor-pointer px-10 py-1 rounded hover:bg-blfs-teal hover:text-white text-center text-xl"
+                                >
+                                    {({active}) => (
+                                        <div
+                                            className={`${active && "bg-blfs-teal text-white"}`}
+                                        >
+                                            {option.ingredient_name}
+                                        </div>
+                                    )}
+                                </Listbox.Option>
+                            ))}
+                        </Listbox.Options>
+                    </Transition>
+                </Listbox>
+            </div>
+        </div>
     )
 }
--- components/QuantityOption.js
+++ components/QuantityOption.js
@@ -2,16 +2,26 @@ import { useState, useEffect } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { ChevronUpDownIcon } from '@heroicons/react/20/solid';
 
-export default function DrinkOption({ selectedDrink, setSelectedDrink, menu }) {
-    console.log("selectedDrink:", selectedDrink)
+export default function QuantityOption({ customization }) {
+    const [options, setOptions] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+    const [selectedOption, setSelectedOption] = useState(4);
+
+    const { customization_name, customization_label, customization_ingredient } = customization;
+
+    if (customization_ingredient === "amount") {
+        setOptions(["none", "light", "normal", "extra"]);
+        setSelectedOption("normal");
+    }
+
+    console.log("options:", options);
 
     return (
         <div className="w-full flex flex-nowrap justify-between my-2">
-            <label className="text-xl">Drink</label>
+            <label className="text-xl">{customization_label}</label>
             <div className="">
-                <Listbox value={selectedDrink} onChange={setSelectedDrink}>
+                <Listbox value={selectedOption} onChange={setSelectedOption}>
                     <Listbox.Button className="border text-xl pr-1 pl-3 py-1 rounded flex flex-nowrap justify-between items-center gap-3">
-                        {selectedDrink.drink_name}
+                        {selectedOption && selectedOption.ingredient_name}
                         <ChevronUpDownIcon
                             className="h-5 w-5 text-gray-400"
                             aria-hidden="true"
@@ -26,18 +36,17 @@ export default function DrinkOption({ selectedDrink, setSelectedDrink, menu }) {
                         leaveTo="transform scale-95 opacity-0"
                     >
                         <Listbox.Options className="z-10 shadow absolute bg-white rounded max-h-64 overflow-auto">
-                            {menu.map((drink) => (
+                            {options.length > 1 && options.map((option) => (
                                 <Listbox.Option
-                                    key={drink.id}
-                                    value={drink}
-                                    disabled={!drink.in_stock}
+                                    key={option}
+                                    value={option}
                                     className="hover:cursor-pointer px-10 py-1 rounded hover:bg-blfs-teal hover:text-white text-center text-xl"
                                 >
                                     {({active}) => (
                                         <div
                                             className={`${active && "bg-blfs-teal text-white"}`}
                                         >
-                                            {drink.drink_name}
+                                            {option}
                                         </div>
                                     )}
                                 </Listbox.Option>
