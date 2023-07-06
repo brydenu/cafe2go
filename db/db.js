7c8ffedb604ef2bfcff6003ba30b44a0a3560d05
brydenu <brydenu@gmail.com>
2023-07-06 09:48:04 -0700

    Removed env

--- db/db.js
+++ db/db.js
@@ -1,11 +1,29 @@
 const { Pool } = require("pg");
 
+const ENV = process.env.ENVIRONMENT;
+
+let user, host, database, password, port;
+
+if (ENV === "prod") {
+  user = process.env.DB_USER;
+  host = process.env.DB_HOST;
+  database = process.env.DB_NAME;
+  password = process.env.DB_PASS;
+  port = process.env.DB_PORT;
+} else {
+  user = 'bryden';
+  host = 'localhost';
+  database = 'blfs_cafe';
+  password = '';
+  port = 5432;
+}
+
 const pool = new Pool({
-    user: 'bryden',
-    host: 'localhost',
-    database: 'blfs_cafe',
-    password: '',
-    port: 5432, // default port for PostgreSQL
+    user: user,
+    host: host,
+    database: database,
+    password: password,
+    port: port,
   });
   
 
