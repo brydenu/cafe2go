0072a524eda9fc32c4ce32f98448eb6ef3035779
brydenu <brydenu@gmail.com>
2023-09-21 14:06:12 -0700

    Commented/removed deprecated dev/prod branching logic for db selection

--- utils/db/orders/getUserCurrentOrder.js
+++ utils/db/orders/getUserCurrentOrder.js
@@ -3,56 +3,56 @@ import { supabase } from "db/db";
 import { isWithinInterval, parseISO, subHours } from "date-fns";
 
 export default async function getUserCurrentOrder(id) {
-  if (process.env.ENVIRONMENT === "dev") {
-    // Original code
-    const res = await pool.query(`
-        SELECT * from orders
-        WHERE user_id = ${id}
-        ORDER BY order_id DESC
-        LIMIT 1;
-    `);
-    const order = res.rows[0];
-    const currentDate = new Date();
-    const twoHoursAgo = subHours(currentDate, 1);
-    const isWithinLastTwoHours = isWithinInterval(order?.ordered_date, {
-      start: twoHoursAgo,
-      end: currentDate,
-    });
-    if (order?.inProgress || isWithinLastTwoHours) {
-      return order;
-    } else {
-      return null;
-    }
-  } else {
-    // Supabase code
-    const { data, error } = await supabase
-      .from("orders")
-      .select("*")
-      .eq("user_id", id)
-      .order("order_id", { ascending: false })
-      .limit(1);
+  // if (process.env.ENVIRONMENT === "dev") {
+  //   // Original code
+  //   const res = await pool.query(`
+  //       SELECT * from orders
+  //       WHERE user_id = ${id}
+  //       ORDER BY order_id DESC
+  //       LIMIT 1;
+  //   `);
+  //   const order = res.rows[0];
+  //   const currentDate = new Date();
+  //   const twoHoursAgo = subHours(currentDate, 1);
+  //   const isWithinLastTwoHours = isWithinInterval(order?.ordered_date, {
+  //     start: twoHoursAgo,
+  //     end: currentDate,
+  //   });
+  //   if (order?.inProgress || isWithinLastTwoHours) {
+  //     return order;
+  //   } else {
+  //     return null;
+  //   }
+  // } else {
+  // Supabase code
+  const { data, error } = await supabase
+    .from("orders")
+    .select("*")
+    .eq("user_id", id)
+    .order("order_id", { ascending: false })
+    .limit(1);
 
-    if (error) {
-      console.log("the error happened on searching within supabase");
-    }
-
-    const order = data[0];
-    const currentDate = new Date();
-    // console.log("currentDate", currentDate);
-    const twoHoursAgo = subHours(currentDate, 1);
-    // console.log("oneHourAgo", twoHoursAgo);
-
-    const orderedDate = parseISO(order?.ordered_date);
-    // console.log("orderedDate", orderedDate);
-    const isWithinLastTwoHours = isWithinInterval(orderedDate, {
-      start: twoHoursAgo,
-      end: currentDate,
-    });
-    // console.log("isWithinLastTwoHours", isWithinLastTwoHours);
-    if (order?.inProgress || isWithinLastTwoHours) {
-      return { currentOrder: order };
-    } else {
-      return { currentOrder: null };
-    }
+  if (error) {
+    console.log("the error happened on searching within supabase");
   }
+
+  const order = data[0];
+  const currentDate = new Date();
+  // console.log("currentDate", currentDate);
+  const twoHoursAgo = subHours(currentDate, 1);
+  // console.log("oneHourAgo", twoHoursAgo);
+
+  const orderedDate = parseISO(order?.ordered_date);
+  // console.log("orderedDate", orderedDate);
+  const isWithinLastTwoHours = isWithinInterval(orderedDate, {
+    start: twoHoursAgo,
+    end: currentDate,
+  });
+  // console.log("isWithinLastTwoHours", isWithinLastTwoHours);
+  if (order?.inProgress || isWithinLastTwoHours) {
+    return { currentOrder: order };
+  } else {
+    return { currentOrder: null };
+  }
+  // }
 }
--- utils/db/orders/patchInProgressOrder.js
+++ utils/db/orders/patchInProgressOrder.js
@@ -2,27 +2,27 @@ import { pool } from "db/db";
 import { supabase } from "db/db";
 
 export default async function patchInProgressOrder(orderId, undo = false) {
-  if (process.env.ENVIRONMENT === "dev") {
-    // Original code
-    const response = await pool.query(`
-      UPDATE orders
-      SET in_progress = ${undo}, completed_date = NOW() 
-      WHERE order_id=${orderId}
-    `);
+  // if (process.env.ENVIRONMENT === "dev") {
+  //   // Original code
+  //   const response = await pool.query(`
+  //     UPDATE orders
+  //     SET in_progress = ${undo}, completed_date = NOW()
+  //     WHERE order_id=${orderId}
+  //   `);
 
-    return response.rows[0];
-  } else {
-    // Supabase code
-    const { data, error } = await supabase
-      .from("orders")
-      .update({ in_progress: undo, completed_date: new Date() })
-      .eq("order_id", orderId)
-      .single();
+  //   return response.rows[0];
+  // } else {
+  // Supabase code
+  const { data, error } = await supabase
+    .from("orders")
+    .update({ in_progress: undo, completed_date: new Date() })
+    .eq("order_id", orderId)
+    .single();
 
-    if (error) {
-      // Handle the error
-    }
-
-    return data;
+  if (error) {
+    // Handle the error
   }
+
+  return data;
+  // }
 }
