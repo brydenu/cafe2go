f419cec2b75d8ec79903aabc11d1ba6539ab9ffc
brydenu <brydenu@gmail.com>
2023-09-22 14:20:40 -0700

    Updated tailwind class

--- components/DashboardOption.js
+++ components/DashboardOption.js
@@ -21,7 +21,7 @@ export default function DashboardOption({
     <div
       className={`w-90 h-24 sm:h-32 mx-5 ${
         primaryOption ? " sm:w-full mx-5 " : " sm:w-full sm:min-w-48 sm:px-10 "
-      } sm:h-60 sm:px-10 sm:aspect-square flex flex-col justify-center items-center rounded shadow-md m-2 duration-300 ${
+      } sm:h-60 sm:px-10 flex flex-col justify-center items-center rounded shadow-md m-2 duration-300 ${
         isDisabled ? `bg-gray-500` : `hover:cursor-pointer ${backgroundColor}`
       } `}
       onClick={handleClick}
